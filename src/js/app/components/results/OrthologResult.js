341471a064fb6755fba313c478ee2621c3f10b55
Jim Thurmond <jim.flybase@cox-thurmond.net>
2016-09-15 17:50:30 -0400

    Makes several column headings vertical.  Filters GO terms for experimental evidence codes. Blanks out ortholog call sources when they are irrelevant to target gene species.

--- src/js/app/components/results/OrthologResult.js
+++ src/js/app/components/results/OrthologResult.js
@@ -8,6 +8,26 @@ import './OrthologResult.css';
 import '../ribbon.css';
 
 const sourceTitles = ['Compara','HGNC','Homologene','Inparanoid','Isobase','OMA','OrthoDB','orthoMCL','Panther','Phylome','RoundUp','TreeFam','ZFIN'];
+// matrix of excluded species for each source
+const sourceNAs = {
+      'Compara': ['Spom'],
+      'HGNC': ['Cele', 'Drer', 'Dmel', 'Scer', 'Spom', 'Xtro', 'Rnor'],
+      'Homologene': [],
+      'Inparanoid': [],
+      'Isobase': ['Drer', 'Spom', 'Xtro', 'Rnor'],
+      'OMA': [],
+      'OrthoDB': ['Cele'],
+      'orthoMCL': ['Xtro'],
+      'Panther': [],
+      'Phylome': [],
+      'RoundUp': ['Rnor'],
+      'TreeFam': [],
+      'ZFIN': ['Cele', 'Scer', 'Spom', 'Xtro', 'Rnor']
+};
+
+const sourcesForSpecies = {
+//      'Dmel': 
+};
 
 export default class OrthologResult extends Component {
 
@@ -35,14 +55,17 @@ export default class OrthologResult extends Component {
         return(<span>{reports[0]}</span>)
     }
 
+    parseSpecies(speciesTxt) {
+        return speciesTxt;
+    }
+
     renderSpecies(speciesTxt) {
         if( speciesTxt ) {
             // speciesTxt should look like "Species name (common name[, another common name])"
             var spTokens = speciesTxt.match(/(.*) \((.*)\)/);
             // spTokens[1] is scientific name, spTokens[2] is common name(s)
-            var Gspc = spTokens[1].replace(/^(.).*\s/, "$1. ");
-           // return(<span><i>{Gspc}</i> ({spTokens[2]})</span>);
-            return(<i>{Gspc}</i>);
+            var G_sp = spTokens[1].replace(/^(.).*\s/, "$1. ");
+            return(<i>{G_sp}</i>);
         }
         return null;
     }
@@ -66,12 +89,19 @@ export default class OrthologResult extends Component {
         return null;
     }
 
+    bestScoreTitle() {
+        return(<div className="rotated-text"><span className="rotated-text-inner">Best Score</span></div>);
+    }
+
+    bestReverseTitle() {
+        return(<div className="rotated-text"><span className="rotated-text-inner">Best Reverse</span></div>);
+    }
+
     sourceTitles() {
         let titles = [];
         for (let s of sourceTitles) {
             titles.push(<div className="rotated-text" key={s}><span className="rotated-text-inner">{s}</span></div>);
         }
-
         return titles;
     }
 
@@ -83,7 +113,12 @@ export default class OrthologResult extends Component {
         return titles;
     }
 
-    renderSource(sources) {
+    pubHeaderTitle() {
+        return(<div className="rotated-text"><span className="rotated-text-inner">Publications</span></div>);
+    }
+
+    renderSource(sources,row) {
+        var sp = row.target_species_abbreviation;
         let sourceChecks = [];
         if (sources) {
             for (let s of sourceTitles) {
@@ -91,9 +126,12 @@ export default class OrthologResult extends Component {
                     "paddingRight": "0.2em",
                     "paddingLeft": "0.2em"
                 };
-
-                if (sources.findIndex((el,i) => el.toUpperCase() === s.toUpperCase()) != -1) {
+         //   console.debug('looking for '+sp+' in sourceNAs for '+s+': '+sourceNAs[s]);
+                if(sources.findIndex((el,i) => el.toUpperCase() === s.toUpperCase()) != -1) {
                     sourceChecks.push(<li key={s} style={styles}><Icon name="check-square-o" fixedWidth={true} /></li>);
+                }
+                else if( sourceNAs[s].indexOf(sp) >= 0 ) {
+                    sourceChecks.push(<li key={s} style={styles}><Icon name="ban-circle" fixedWidth={true} /></li>);
                 }
                 else {
                     sourceChecks.push(<li key={s} style={styles}><Icon name="square-o" fixedWidth={true} /></li>);
@@ -114,14 +152,33 @@ export default class OrthologResult extends Component {
         return null;
     }
 
+    GOterms() {
+    }
+
     renderGO( GOobj ) {
-        // GOobj: { C: { name:'Cellular Component', terms: [{},{},...], count: # }, P: {}, F: {} }
+        // filtering for 'experimental evidence codes' [http://geneontology.org/page/guide-go-evidence-codes]: EXP IPI IDA IGI IMP IEP
+        if( GOobj ) {
+            var rxp = /EXP|IPI|IDA|IGI|IMP|IEP/i;
+            for( let b of ['C','F','P'] ) {     // loop over top-level GO branch sub-objects (Cellular Component C, Molecular Function F, Biological Process P)
+                var GObranch = GOobj[b];
+                var newTermsArray = [];
+                for( let t=0; t<GObranch.terms.length; t++ ) {
+                    let termObj = GObranch.terms[t];
+                    if( rxp.test(termObj.evidence_code) ) {
+                        newTermsArray.push(termObj);
+                    }
+                }
+                GOobj[b].terms = newTermsArray;         // comment just this line to 'turn off' filtering
+            }
+        }
         return(<MiniStrip ribbondata={GOobj} />)
-      //  return <div>{Object.keys(GOobj)}</div>;
     }
 
     renderPubs( publications ) {
         return (<span>{publications.length}</span>);
+    }
+
+    score() {
     }
 
     sortByScore(a, b, order, field, score) {
@@ -200,6 +257,7 @@ export default class OrthologResult extends Component {
                     </TableHeaderColumn>
                     <TableHeaderColumn dataField="best_score"
                                        dataAlign="center"
+                                       className="verticalTitlesTH"
                                        width="50"
                                        dataFormat={this.renderCheck}
                                        caretRender={this.renderSortIndicator}
@@ -208,10 +266,13 @@ export default class OrthologResult extends Component {
                         <OverlayTrigger trigger="click" placement="top" overlay={<Popover id="best_score" title="Best Score">Indicates that the orthology call has the highest score when going from the query organism to the target organism.</Popover>}>
                             <a tabindex="0" className="btn" role="button" onClick={(e) => e.stopPropagation() } ><Icon name="info-circle" /></a>
                         </OverlayTrigger>
+                        <br />
+                        {this.bestScoreTitle()}
                     </TableHeaderColumn>
                     <TableHeaderColumn dataField="best_reverse_score"
                                        dataAlign="center"
                                        width="50"
+                                       className="verticalTitlesTH"
                                        dataFormat={this.renderCheck}
                                        caretRender={this.renderSortIndicator}
                                        dataSort={true}
@@ -219,17 +280,20 @@ export default class OrthologResult extends Component {
                         <OverlayTrigger trigger="click" placement="top" overlay={<Popover id="best_reverse_score" title="Best Score - Reverse Search">Indicates that the orthology call has the highest score when going from the target organism to the query organism.</Popover>}>
                             <a tabindex="0" className="btn" role="button" onClick={(e) => e.stopPropagation() } ><Icon name="info-circle" /></a>
                         </OverlayTrigger>
+                        <br />
+                        {this.bestReverseTitle()}
                     </TableHeaderColumn>
                     <TableHeaderColumn dataField="source"
                                        dataAlign="center"
                                        width="330"
                                        className="verticalTitlesTH"
                                        dataFormat={this.renderSource}
+                                       formatExtraData="ortholog_gene_reports"
                                        caretRender={this.renderSortIndicator}
                                        dataSort={true}
                                        sortFunc={this.sortByScore}>
-                            <p>Prediction Dervied From</p>
-                            {this.sourceTitles()}
+                        <p>Prediction Dervied From</p>
+                        {this.sourceTitles()}
                     </TableHeaderColumn>
                     <TableHeaderColumn dataField="GO"
                                        dataAlign="center"
@@ -238,7 +302,7 @@ export default class OrthologResult extends Component {
                                        width="90">
                         <p>
                             GO
-                            <OverlayTrigger trigger="click" placement="top" overlay={<Popover id="GO_terms" title="Gene Ontology">Color intensity indicates number of curated terms. Hover/tap to display exact number.</Popover>}>
+                            <OverlayTrigger trigger="click" placement="top" overlay={<Popover id="GO_terms" title="Gene Ontology">Color intensity indicates number of terms curated with GO experimental evidence codes. Hover/tap to display exact number.</Popover>}>
                                 <a tabindex="0" className="btn" role="button" onClick={(e) => e.stopPropagation() } ><Icon name="info-circle" /></a>
                             </OverlayTrigger>
                         </p>
@@ -246,10 +310,12 @@ export default class OrthologResult extends Component {
                     </TableHeaderColumn>
                     <TableHeaderColumn dataField="publications_number"
                                        caretRender={this.renderSortIndicator}
+                                       className="verticalTitlesTH"
                                        dataSort={true}
                                        dataAlign="center"
-                                       width="100">
-                        Publications
+                                       width="60">
+                        <p>&nbsp;</p>
+                        {this.pubHeaderTitle()}
                     </TableHeaderColumn>
                 </BootstrapTable>
             </div>
